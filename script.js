65c46539101479b3644d5aebbf6948de63850d0d
ahamedfayaz10 <ahamedfayaz854@gmail.com>
2024-07-26 23:48:04 +0530

    todo list created

---
+++ script.js
@@ -0,0 +1,24 @@
+
+var inputbox = document.getElementById("input-box")
+var ul = document.getElementById("list-container")
+var checkbox = ul.querySelectorAll("input[type='checkbox']")
+
+
+function add() {
+
+    if (inputbox.value === "") {
+        alert("You must write Something!")
+    }
+    else {
+        var listitem = document.createElement("li")
+        listitem.innerHTML = "<input type='checkbox'>" + inputbox.value + "<button onclick='deleteitem(event)'>\u00d7</button>"
+        ul.append(listitem)
+        inputbox.value = ""
+    }
+}
+
+
+function deleteitem(event) {
+    event.target.parentElement.remove()
+}
+
